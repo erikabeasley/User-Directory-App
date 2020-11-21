3d6832ff19fbe6f9d63fa34b04881e07014aaab7
Erika <erika@beasleys.com>
2020-11-21 12:40:24 -0500

    style

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export default{ 
-    getUsers:function(){    
+export default {
+  // Grabs the users from our API via Axios
+  getUsers: function() {
 
-        return axios.get("https://randomuser.me/api/?results=200&nat=us");
-    }
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  }
 };
